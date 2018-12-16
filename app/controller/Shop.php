070c69edd34968ec33cfeb6c4797edbea8394250
吳姿萱 <cherry51120@gmail.com>
2018-12-16 11:34:54 +0800

    search page

--- app/controller/Shop.php
+++ app/controller/Shop.php
@@ -10,7 +10,7 @@ class Shop extends SitePageController {
     wtfTo('ShopIndex');
 
     if (in_array(Router::methodName(), ['index', 'rank', 'search'])) {
-      $this->rightMenu = View::create('site/Part/rightmenu.php')->with('areas', [1])->with('foods', []);
+      $this->rightMenu = View::create('site/Part/rightmenu.php')->with('get', $_GET);
     }
 
     if (in_array(Router::methodName(), ['show']))
@@ -43,7 +43,7 @@ class Shop extends SitePageController {
 
   public function search() {
     $asset = $this->asset->addCSS('/asset/css/site/search.css');
-    $navs = [['name' => '首頁', 'href' => '/'], ['name' => '美食排行榜']];
+    $navs = [['name' => '首頁', 'href' => '/'], ['name' => '美食查詢結果']];
 
     $total = \M\ShopMain::count();
     $pagesStr = Pagination::info($total, 20, 2);
@@ -58,8 +58,8 @@ class Shop extends SitePageController {
                       ->with('rightMenu', $this->rightMenu)
                       ->with('pagesStr', $pagesStr['links'])
                       ->with('navs', $navs)
-                      ->with('title', '美食搜尋')
-                      ->with('hr', '美食搜尋結果');
+                      ->with('title', '美食查詢結果')
+                      ->with('hr', '美食查詢');
   }
 
   public function show() {
--- app/view/site/Part/rightmenu.php
+++ app/view/site/Part/rightmenu.php
@@ -3,13 +3,15 @@
     <div class="searchArea">
         <p class="typeTitle">地點</p>
         <ul class="main">
-            <?php echo implode('', array_map(function($main) use ($areas) {
+            <?php 
+                $areas = isset($get['area']) ? $get['area'] : [];
+                echo implode('', array_map(function($main) use ($areas) {
                     if(!$main->subs) return '';
 
                     $all = '<li class="all-pick"><input type="checkbox"><label>全' . $main->name . '</label></li>';
                     $subs = '<ul class="sub">' . $all . implode('', array_map(function($sub) use ($areas, &$active) {
                                 $active = $active || in_array($sub->id, $areas) ? 'is-active' : ''; 
-                                return '<li><input type="checkbox" id="' . $sub->id . '" name="area[]" value="' . $sub->id . '" ' . (in_array($sub->id, $areas) ? 'checked' : '') . '><label for="' . $sub->id . '">' . $sub->name . '</label></li>';
+                                return '<li><input type="checkbox" id="area' . $sub->id . '" name="area[]" value="' . $sub->id . '" ' . (in_array($sub->id, $areas) ? 'checked' : '') . '><label for="area' . $sub->id . '">' . $sub->name . '</label></li>';
                             }, $main->subs )) . '</ul>';
                     
                     return '<li class="m-list ' . $active . '"><p class="mainList"><span>' . $main->name .'</span></p>' . $subs . '</li>';
@@ -21,13 +23,15 @@
     <div class="searchFood">
         <p class="typeTitle">分類</p>
         <ul class="main">
-            <?php echo implode('', array_map(function($main) use ($foods) {
+            <?php 
+                $foods = isset($get['food']) ? $get['food'] : [];
+                echo implode('', array_map(function($main) use ($foods) {
                     if(!$main->subs) return '';
 
                     $all = '<li class="all-pick"><input type="checkbox"><label>全' . $main->name . '</label></li>';
                     $subs = '<ul class="sub">' . $all . implode('', array_map(function($sub) use ($foods, &$active) {
                                 $active = $active || in_array($sub->id, $foods) ? 'is-active' : ''; 
-                                return '<li><input type="checkbox" id="' . $sub->id . '" name="food[]" value="' . $sub->id . '" ' . (in_array($sub->id, $foods) ? 'checked' : '') . '><label for="' . $sub->id . '">' . $sub->name . '</label></li>';
+                                return '<li><input type="checkbox" id="food' . $sub->id . '" name="food[]" value="' . $sub->id . '" ' . (in_array($sub->id, $foods) ? 'checked' : '') . '><label for="food' . $sub->id . '">' . $sub->name . '</label></li>';
                             }, $main->subs )) . '</ul>';
                     
                     return '<li class="m-list ' . $active . '"><p class="mainList"><span>' . $main->name .'</span></p>' . $subs . '</li>';
